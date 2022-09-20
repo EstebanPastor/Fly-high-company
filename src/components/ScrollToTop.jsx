9c85aedf56b77445a20e6efc4eda7ec0ffa5d39e
EstebanPastor <estebanandrespastor@gmail.com>
2022-09-20 05:01:00 -0300

    Scroll to top added

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
-
-const ScrollToTop = () => {
+import React, { useState } from "react";
+import styled from "styled-components";
+import topLogo from "../assets/toTop.png";
+export default function ScrollToTop() {
+  const [scrollState, setScrollState] = useState(false);
+  const toTop = () => {
+    window.scrollTo({ top: 0 });
+  };
+  window.addEventListener("scroll", () => {
+    window.pageYOffset > 200 ? setScrollState(true) : setScrollState(false);
+  });
   return (
-    <div>
-    </div>
-  )
+    <ToTop onClick={toTop} scrollState={scrollState}>
+      <img src={topLogo} alt="" />
+    </ToTop>
+  );
 }
 
-export default ScrollToTop
+const ToTop = styled.div`
+  display: ${({ scrollState }) => (scrollState ? "block" : "none")};
+  position: fixed;
+  cursor: pointer;
+  z-index: 10;
+  bottom: 1.3rem;
+  right: 1.9rem;
+  img {
+    height: 2.8rem;
+    border-radius: 1.4rem;
+  }
+  border-radius: 2rem;
+  background-color: #000000;
+  padding: 1rem;
+`;
